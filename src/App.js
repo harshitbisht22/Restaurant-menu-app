6a7a75cee73470bbf8c6adb2664703bb8354be8c
Harshit Bisht <Harshit.bisht.22@gmail.com>
2023-03-02 13:05:52 +0530

    New Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Resturant />
+    
+    </div>
+  )
+}
+
+export default App
